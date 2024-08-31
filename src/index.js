1312816f7748a55d199f23bdd950240050bbe436
japtor <gorariagroup@gmail.com>
2024-08-31 13:40:33 +0700

    Japtor

--- src/index.js
+++ src/index.js
@@ -1,22 +1,33 @@
+const path = require('path')
 const express = require('express')
 // import express from 'express'
 const morgan = require('morgan')
-// import morgan from 'morgan'
-const handlebars = require('express-handlebars')
+const handlebars = require('express-handlebars').create({
+    defaultLayout: 'main',
+    extname: '.hbs'
+})
 // import handlebars from 'express-handlebars' // exphbs
 const app = express()
 const port = 3000
 
+// HTTP Logger
 app.use(morgan('combined'))
 
-app.engine('handlebars', handlebars())
-app.set('view engine', 'handlebars');
-app.set('views', './views');
+// Template engine
+app.engine('.hbs', handlebars.engine())
+app.set('view engine', '.hbs');
+app.set('views', './resources/views');
 
 app.get('/', (req, res) => {
-    res.send('Gorth Inc.')
+    // res.send('Gorth Inc.')
+    res.render('home');
 })
+
+app.get('/news', (req, res) => {
+    res.render('news');
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening at http://localhost:${port}`);
 })
