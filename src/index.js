f6dce58395686021355ff3107ad25bf459242d9f
japtor <gorariagroup@gmail.com>
2024-08-31 15:02:20 +0700

    Japtor

--- src/index.js
+++ src/index.js
@@ -19,8 +19,9 @@ app.engine('.hbs', handlebars.engine({
     defaultLayout: 'main',
     extname: '.hbs'
 }))
-app.set('view engine', '.hbs');
-app.set('views', './resources/views');
+app.set('view engine', '.hbs')
+// app.set('views', './resources/views')
+app.set('views', path.join(__dirname, 'resources/views'))
 
 app.get('/', (req, res) => {
     // res.send('Gorth Inc.')
@@ -33,5 +34,5 @@ app.get('/news', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-    console.log(`Example app listening at http://localhost:${port}`);
+    // console.log(`Example app listening at http://localhost:${port}`);
 })
