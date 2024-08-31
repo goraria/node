600dc208045cb61e1d9ba801c2b4f0d114a2a272
japtor <gorariagroup@gmail.com>
2024-09-01 06:48:05 +0700

    Japtor

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
 
+app.use(express.static(path.join(__dirname, './public')))
 // HTTP Logger
 app.use(morgan('combined'))
 
