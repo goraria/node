a4915e26101ac959ca3217f270b0b9d25e0654fa
japtor <gorariagroup@gmail.com>
2024-08-31 12:15:52 +0700

    Japtor

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express = require('express')
+// import express from 'express'
 const morgan = require('morgan')
+// import morgan from 'morgan'
+const handlebars = require('express-handlebars')
+// import handlebars from 'express-handlebars' // exphbs
 const app = express()
 const port = 3000
 
 app.use(morgan('combined'))
+
+app.engine('handlebars', handlebars())
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 app.get('/', (req, res) => {
     res.send('Gorth Inc.')
